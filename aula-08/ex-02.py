8f023d647a1cdea8d85136febb7edc1b2c3df625
Roberto <robertofurini12@gmail.com>
2021-03-31 14:42:52 -0300

    aula-08

---
+++ aula-08/ex-02.py
@@ -0,0 +1,3 @@
+import random
+n = random.randint(1, 10)
+print(n)
---
+++ aula-08/exercicio-01.py
@@ -0,0 +1,4 @@
+from math import sqrt
+num = int(input('Digite um numero: '))
+raiz = sqrt(num)
+print('A raiz vale {:.3f}' .format(raiz))
