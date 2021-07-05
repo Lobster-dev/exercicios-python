f36b453461e738db6a49b4be0f953fc26ee50557
Lobster-dev <80717591+Lobster-dev@users.noreply.github.com>
2021-07-05 17:09:33 -0300

    Delete aula-14 directory

--- aula-14/ex-60/ex-60.py
+++
@@ -1,9 +0,0 @@
-# Fatorial
-numero = int(input('Digite um número para saber sua fatorial: '))
-contador = numero
-fatorial = numero
-while 1 < contador <= numero:
-    fatorial = fatorial * (contador-1)
-    contador = contador - 1
-
-print(fatorial)
