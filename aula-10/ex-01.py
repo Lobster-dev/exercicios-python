15f026e147a4d13a634960764a7ffa32a3305726
Lobster-dev <80717591+Lobster-dev@users.noreply.github.com>
2021-07-05 17:07:20 -0300

    Delete aula-10 directory

--- aula-10/ex-01.py
+++
@@ -1,8 +0,0 @@
-n1 = float(input('Qual a sua 1° nota: '))
-n2 = float(input('Qual a sua 2° nota: '))
-media = (n1 + n2)/2
-if media>=6.5:
-    print('você está aprovado!!')
-else:
-    print('Você está de recuperação :( ')
-print('Sua media foi de {}'.format(media))
--- aula-10/ex-34/ex-34.py
+++
@@ -1,7 +0,0 @@
-salario = float(input('Qual o valor do funcionario: '))
-if salario <= 1250:
-    juros = (salario*0.15)+salario
-    print('Quem ganhava {} passa a ganhar {}'.format(salario, juros))
-else:
-    juros = (salario*0.10)+salario
-    print('Quem ganhava {} passa a ganhar {}'.format(salario, juros))
