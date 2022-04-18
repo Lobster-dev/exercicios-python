50ba9aba5e7ad974588c66ffd1ce14b831f9b3a3
Lobster-dev <80717591+Lobster-dev@users.noreply.github.com>
2022-04-18 19:15:27 -0300

    add f string method

--- aula-05-new/exercicio-02.py
+++ aula-05-new/exercicio-02.py
@@ -1,3 +1,8 @@
 # Você escreve o seu nome e ele te responde com uma mensagem de boas vindas
 nome = input('Digite o seu nome: ')
 print('É um prazer te conhecer, {}' .format(nome))
+
+
+# método utilizando F string
+nome = input('Digite o seu nome: ')
+print(f'É um prazer te conhecer, {nome}')
